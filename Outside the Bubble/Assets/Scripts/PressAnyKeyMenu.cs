21def40be390d7df3e7eb916e77e720ebf9c928e
r00tVegetable <bob.gaming.cro@gmail.com>
2025-01-25 16:34:08 +0100

    UI

--- Outside the Bubble/Assets/Scripts/PressAnyKeyMenu.cs
+++ Outside the Bubble/Assets/Scripts/PressAnyKeyMenu.cs
@@ -1,6 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PressAnyKeyMenu
+public class PressAnyKeyMenu : MonoBehaviour
 {
-    
+    public string sceneName; // The name of the scene to load
+
+    // Update method for detecting any key press
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("A key or mouse click has been detected in Update");
+            LoadScene();
+        }
+    }
+
+    // Function to load the scene (can be called by a UI button)
+    public void OnButtonPress()
+    {
+        Debug.Log("Button pressed to load the scene");
+        LoadScene();
+    }
+
+    // Centralized method to load the scene
+    private void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Scene name is not set in the inspector!");
+        }
+    }
 }
